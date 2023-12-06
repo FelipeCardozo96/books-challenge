3ec253216098e69f37b2b21f153393c5516e7dd5
FelipeCardozo96 <felipemcr996@gmail.com>
2023-12-06 14:44:17 -0300

    Se instalaron dependecias de middlewares, se creó el middleware de session

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const mainRouter = require('./routes/main');
-
+const session = require('express-session');
+const methodOverride = require('method-override');
+const cookiesMiddleware = require('./middlewares/cookiesMiddleware');
+const sessionMiddleware = require('./middlewares/sessionMiddleware');
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(cookieParser())
+app.use(session({secret: 'booksApp'}));
+app.use(cookieMiddleware);
+app.use(sessionMiddleware);
 
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
